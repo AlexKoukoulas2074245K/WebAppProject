f5cc24fbc2b2a14719bacdf70ef64dbeb996d0b5
Sayyad <sayyadr@hotmail.co.uk>
2015-01-23 19:24:39 +0000

    Added forgotten chapter 4 exercise from sheet

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,5 +6,5 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    context_dict = {'boldmessage': "This tutorial has been put together by Mohammed Aslam, 2086225A"}
     return render(request, 'rango/about.html', context_dict)
