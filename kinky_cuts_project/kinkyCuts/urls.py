a749e7f4852fa5d27ab3d081013b85a24b6f0768
unknown <2074245k@BO715-11-04.ad.dcs.gla.ac.uk>
2015-03-04 15:59:35 +0000

    Added all views, templates and urls

--- kinky_cuts_project/kinkyCuts/urls.py
+++ kinky_cuts_project/kinkyCuts/urls.py
@@ -2,4 +2,11 @@ from django.conf.urls import patterns,url
 from kinkyCuts import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'))
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^explore/$', views.explore, name='explore'),
+    url(r'^mypictures/$', views.mypictures, name='mypictures'),
+    url(r'^myaccount/$', views.myaccount, name='myaccount'),
+    url(r'^help/$', views.helpage, name='help'),
+    url(r'^sign/$', views.sign, name='sign'),
+    )
--- kinky_cuts_project/kinkyCuts/views.py
+++ kinky_cuts_project/kinkyCuts/views.py
@@ -4,5 +4,29 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context_dict = {};
-    return render(request, 'kinkyCuts/index.html', context_dict);
+    context_dict = {}
+    return render(request, 'kinkyCuts/index.html', context_dict)
+
+def about(request):
+    context_dict = {}
+    return render(request, 'kinkyCuts/about.html', context_dict)
+
+def explore(request):
+    context_dict = {}
+    return render(request, 'kinkyCuts/explore.html', context_dict)
+
+def mypictures(request):
+    context_dict = {}
+    return render(request, 'kinkyCuts/mypictures.html', context_dict)
+
+def myaccount(request):
+    context_dict = {}
+    return render(request, 'kinkyCuts/myaccount.html', context_dict)
+
+def helpage(request):
+    context_dict = {}
+    return render(request, 'kinkyCuts/help.html', context_dict)
+
+def sign(request):
+    context_dict = {}
+    return render(request, 'kinkyCuts/sign.html', context_dict)
