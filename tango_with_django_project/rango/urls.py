d4408f11a32135287e29ecdd7bbcf1cdbf53e8be
Sayyad <sayyadr@hotmail.co.uk>
2015-03-09 16:46:16 +0000

    Chapter 16+17 - Completed

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -5,6 +5,7 @@ from rango import views
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^about/', views.about, name='about'),
+        url(r'^goto/$', views.track_url, name='goto'),
         url(r'^add_category/$', views.add_category, name='add_category'),
         url(r'^restricted/', views.restricted, name='restricted'),
         url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -7,6 +7,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from datetime import datetime
+from rango.bing_search import run_query
+from django.shortcuts import redirect
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -52,18 +54,31 @@ def about(request):
 
 def category(request, category_name_slug):
     context_dict = {}
+    context_dict['result_list'] = None
+    context_dict['query'] = None
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+            context_dict['result_list'] = result_list
+            context_dict['query'] = query
 
     try:
         category = Category.objects.get(slug=category_name_slug)
         context_dict['category_name'] = category.name
-
-        pages = Page.objects.filter(category=category)
-        context_dict['category_name_slug'] = category_name_slug
+        pages = Page.objects.filter(category=category).order_by('-views')
         context_dict['pages'] = pages
         context_dict['category'] = category
     except Category.DoesNotExist:
         pass
-    return render(request,'rango/category.html',context_dict)
+
+    if not context_dict['query']:
+        context_dict['query'] = category.name
+
+    return render(request, 'rango/category.html', context_dict)
 
 @login_required
 def add_category(request):
@@ -121,3 +136,32 @@ from rango.forms import UserForm, UserProfileForm
 @login_required
 def restricted(request):
     return render(request,'rango/restricted.html')
+
+def search(request):
+
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render(request, 'rango/search.html', {'result_list': result_list})
+
+def track_url(request):
+    page_id = None
+    url = '/rango/'
+    if request.method == 'GET':
+        if 'page_id' in request.GET:
+            page_id = request.GET['page_id']
+            try:
+                page = Page.objects.get(id=page_id)
+                page.views = page.views + 1
+                page.save()
+                url = page.url
+            except:
+                pass
+
+    return redirect(url)
